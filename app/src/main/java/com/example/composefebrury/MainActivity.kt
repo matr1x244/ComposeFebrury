5c840945c1c2d5acfdce4c43eaea90f26a48de48
matr1x244 <zio-21@yandex.ru>
2023-01-28 21:42:08 +0300

    Добавил условие if else как работает в compose при комбинировании

--- app/src/main/java/com/example/composefebrury/MainActivity.kt
+++ app/src/main/java/com/example/composefebrury/MainActivity.kt
@@ -12,7 +12,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HomeScreen()
+            HomeScreen(listOf("one", "two", "three"))
+//            HomeScreen(emptyList())
         }
     }
 }
--- app/src/main/java/com/example/composefebrury/home/HomeScreen.kt
+++ app/src/main/java/com/example/composefebrury/home/HomeScreen.kt
@@ -4,32 +4,42 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.Start
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 
 @Composable
-fun HomeScreen() {
-    Row(verticalAlignment = Alignment.CenterVertically) {
-        Box {
-            Text(
-                "Text Box Layout",
-                fontSize = 20.sp,
-                modifier = Modifier.align(Alignment.BottomCenter)
-            )
-        }
-        Spacer(modifier = Modifier.width(20.dp))
-        Column (Modifier.padding(20.dp)){
-            Text("Row Main, Column LinerLayout")
-        }
+fun HomeScreen(list: List<String>) {
+    if (list.isEmpty()) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Box(
+                contentAlignment = Alignment.Center, modifier = Modifier
+                    .fillMaxSize()
 
+            ) {
+                Text("Box list emptyList...")
+            }
+        }
+    } else {
+        Column(
+            horizontalAlignment = Alignment.Start, modifier = Modifier.fillMaxSize()
+        ) {
+            for (string in list) {
+                Text(
+                    "$string",
+                    Modifier
+                        .padding(5.dp)
+                        .align(Start)
+                )
+            }
+        }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 private fun HomeScreenView() {
-    HomeScreen()
+    HomeScreen(listOf("one", "two", "three"))
 }
