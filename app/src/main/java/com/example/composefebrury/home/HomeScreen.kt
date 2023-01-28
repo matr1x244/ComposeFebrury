0241c52a1c7254d42742e9eabf434b9eed23fd1e
matr1x244 <zio-21@yandex.ru>
2023-01-28 21:44:21 +0300

    добавил в strings.xml, текстовое поле

--- app/src/main/java/com/example/composefebrury/home/HomeScreen.kt
+++ app/src/main/java/com/example/composefebrury/home/HomeScreen.kt
@@ -6,8 +6,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.Start
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.composefebrury.R
 
 
 @Composable
@@ -19,7 +21,7 @@ fun HomeScreen(list: List<String>) {
                     .fillMaxSize()
 
             ) {
-                Text("Box list emptyList...")
+                Text(stringResource(R.string.box_lise_empty))
             }
         }
     } else {
