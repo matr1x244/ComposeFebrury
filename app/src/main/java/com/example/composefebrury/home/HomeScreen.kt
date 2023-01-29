2b96cf2b9d8203305676c0c2ef9923522e7a6681
matr1x244 <zio-21@yandex.ru>
2023-01-29 19:34:52 +0300

    Добавил в coil картинку с инета, сделал анимацию, добавил кликабельность на иконку вращения.

--- app/src/main/java/com/example/composefebrury/home/HomeScreen.kt
+++ app/src/main/java/com/example/composefebrury/home/HomeScreen.kt
@@ -1,21 +1,31 @@
 package com.example.composefebrury.home
 
 
+import android.widget.Toast
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Icon
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.example.composefebrury.R
 
 
 @Composable
@@ -37,14 +47,58 @@ fun HomeScreen() {
             modifier = Modifier.align(CenterHorizontally),
             tint = Color.Green
         )
-        var url = "https://гибдд12.рф/img/car__0_.webp"
+
+        val url =
+            "https://sun9-6.userapi.com/s/v1/if1/rYpnK68LRBKov1gS3Hsn5FNtATDdEibSY8koi8COayaTi7VeJLON80zLHLlYK6u1mYzWiLRR.jpg?size=2030x2030&quality=96&crop=0,7,2030,2030&ava=1"
+
+        val infiniteTransition = rememberInfiniteTransition() //анимация вращения
+        val angle by infiniteTransition.animateFloat( // задаем параметры
+            initialValue = 0F,
+            targetValue = 360F,
+            animationSpec = infiniteRepeatable(
+                animation = tween(2000, easing = LinearEasing)
+            )
+        )
+        val context = LocalContext.current
+
         AsyncImage(
-            model = url, contentDescription = null,
-            modifier = Modifier.align(CenterHorizontally)
-                .fillMaxWidth()
-                .padding(10.dp)
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(url)
+                .crossfade(true)
+                .crossfade(1500)
+                .build(),
+            placeholder = painterResource(R.drawable.ic_launcher_background), // фон если нет ответа от сервера
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(200.dp)
+                .clip(CircleShape)
+                .align(CenterHorizontally)
+                .border(
+                    5.dp,
+                    Brush.linearGradient(
+                        colors = listOf(
+                            Color.LightGray,
+                            Color.Green,
+                            Color.White,
+                            Color.Blue
+                        )
+                    ),
+                    CircleShape
+                )
+                .rotate(50f)
+                .graphicsLayer {
+                    rotationZ = angle // подключаем вращение
+                }
+                .clickable {
+                    println("@@@ CLICKER IMAGE")
+                    Toast
+                        .makeText(context, "CLICK ${url.toString()}", Toast.LENGTH_SHORT)
+                        .show()
+                }
         )
     }
+
 }
 
 @Preview(showBackground = true)
