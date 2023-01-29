f9cecbb2d522380e3f5b5d1725b1561384637ae3
matr1x244 <zio-21@yandex.ru>
2023-01-29 18:23:07 +0300

    Image и Icon выравние в column

--- app/src/main/java/com/example/composefebrury/home/HomeScreen.kt
+++ app/src/main/java/com/example/composefebrury/home/HomeScreen.kt
@@ -3,12 +3,13 @@ package com.example.composefebrury.home
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -17,21 +18,21 @@ import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun HomeScreen() {
-    Box(
-        contentAlignment = Alignment.BottomCenter,
-        modifier = Modifier.fillMaxSize()
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
             .background(Color.LightGray)
     ) {
         Image(
             painter = painterResource(id = com.example.composefebrury.R.mipmap.image_marker),
             contentDescription = null,
             modifier = Modifier.fillMaxWidth()
-                .align(Alignment.Center)
+
         )
         Icon(
             painter = painterResource(id = com.example.composefebrury.R.drawable.ic_launcher_foreground),
             contentDescription = null,
-            modifier = Modifier.align(Alignment.Center)
+            modifier = Modifier.align(CenterHorizontally)
         )
     }
 }
