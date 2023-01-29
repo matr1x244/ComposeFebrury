81372fe1e7f191fa1d21cbc48a2103467e3aeb00
matr1x244 <zio-21@yandex.ru>
2023-01-29 17:44:17 +0300

    Создал Box

--- app/src/main/java/com/example/composefebrury/home/HomeScreen.kt
+++ app/src/main/java/com/example/composefebrury/home/HomeScreen.kt
@@ -1,47 +1,30 @@
 package com.example.composefebrury.home
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.Start
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.composefebrury.R
 
 
 @Composable
-fun HomeScreen(list: List<String>) {
-    if (list.isEmpty()) {
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Box(
-                contentAlignment = Alignment.Center, modifier = Modifier
-                    .fillMaxSize()
-
-            ) {
-                Text(stringResource(R.string.box_lise_empty))
-            }
-        }
-    } else {
-        Column(
-            horizontalAlignment = Alignment.Start, modifier = Modifier.fillMaxSize()
-        ) {
-            for (string in list) {
-                Text(
-                    "$string",
-                    Modifier
-                        .padding(5.dp)
-                        .align(Start)
-                )
-            }
-        }
+fun HomeScreen() {
+    Box(
+        contentAlignment = Alignment.BottomCenter,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Box(modifier = Modifier
+            .padding(bottom = 15.dp)
+            .width(200.dp).height(200.dp)
+            .background(Color.Blue))
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 private fun HomeScreenView() {
-    HomeScreen(listOf("one", "two", "three"))
+    HomeScreen()
 }
