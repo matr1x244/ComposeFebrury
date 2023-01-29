dea98971bc869336a798398102c6f205e3b7da4b
matr1x244 <zio-21@yandex.ru>
2023-01-29 18:30:59 +0300

    Добавил библиотеку Coil и AsyncImage

--- app/src/main/java/com/example/composefebrury/home/HomeScreen.kt
+++ app/src/main/java/com/example/composefebrury/home/HomeScreen.kt
@@ -6,14 +6,16 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 
 
 @Composable
@@ -34,6 +36,13 @@ fun HomeScreen() {
             contentDescription = null,
             modifier = Modifier.align(CenterHorizontally),
             tint = Color.Green
+        )
+        var url = "https://гибдд12.рф/img/car__0_.webp"
+        AsyncImage(
+            model = url, contentDescription = null,
+            modifier = Modifier.align(CenterHorizontally)
+                .fillMaxWidth()
+                .padding(10.dp)
         )
     }
 }
