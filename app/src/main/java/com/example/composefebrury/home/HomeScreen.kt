b07c6cf01294f4d989844b4eda3744fb8ffc44d2
matr1x244 <zio-21@yandex.ru>
2023-01-29 17:53:36 +0300

    Убрали фон у box сделали рамку у box

--- app/src/main/java/com/example/composefebrury/home/HomeScreen.kt
+++ app/src/main/java/com/example/composefebrury/home/HomeScreen.kt
@@ -1,12 +1,11 @@
 package com.example.composefebrury.home
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -26,13 +25,26 @@ fun HomeScreen() {
                 .height(200.dp)
 //            .background(Color.Blue, shape = RoundedCornerShape(10.dp))) // закругляем края
 //            .background(Color.Blue, shape = CircleShape)) // либо делаем круг
-                .background(
-                    brush = Brush.linearGradient( // можно задать градиент box
-                        colors = listOf(Color.Blue, Color.DarkGray, Color.Magenta)),
-
-                    alpha = 0.4f,
-                    shape = RoundedCornerShape(10.dp)
+//                .background(
+//                    brush = Brush.linearGradient( // можно задать градиент box
+//                        colors = listOf(Color.Blue, Color.DarkGray, Color.Magenta)),
+//
+//                    alpha = 0.4f,
+//                    shape = RoundedCornerShape(10.dp)
+//                )
+                .border( // можно так задать рамку у фона
+                    width = 5.dp,
+                    brush = Brush.linearGradient(
+                        colors = listOf(
+                            Color.Red,
+                            Color.Green,
+                            Color.Yellow
+                        )
+                    ),
+                    shape = RoundedCornerShape(15.dp)
                 )
+
+
         )
     }
 }
