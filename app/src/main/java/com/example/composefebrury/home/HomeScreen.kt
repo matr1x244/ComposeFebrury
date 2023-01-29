1d0e4bdbaf9455833de02b883a6ccadc46022378
matr1x244 <zio-21@yandex.ru>
2023-01-29 18:21:24 +0300

    Image и Icon принцип работы

--- app/src/main/java/com/example/composefebrury/home/HomeScreen.kt
+++ app/src/main/java/com/example/composefebrury/home/HomeScreen.kt
@@ -1,16 +1,18 @@
 package com.example.composefebrury.home
 
+
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 
 
 @Composable
@@ -18,34 +20,18 @@ fun HomeScreen() {
     Box(
         contentAlignment = Alignment.BottomCenter,
         modifier = Modifier.fillMaxSize()
+            .background(Color.LightGray)
     ) {
-        Box(
-            modifier = Modifier
-                .padding(bottom = 15.dp)
-                .width(200.dp)
-                .height(200.dp)
-                .background(Color.Blue, shape = RoundedCornerShape(15.dp)) // закругляем края
-//            .background(Color.Blue, shape = CircleShape)) // либо делаем круг
-//                .background(
-//                    brush = Brush.linearGradient( // можно задать градиент box
-//                        colors = listOf(Color.Blue, Color.DarkGray, Color.Magenta)),
-//
-//                    alpha = 0.4f,
-//                    shape = RoundedCornerShape(10.dp)
-//                )
-                .border( // можно так задать рамку у фона
-                    width = 5.dp,
-                    brush = Brush.linearGradient(
-                        colors = listOf(
-                            Color.Red,
-                            Color.Green,
-                            Color.Yellow
-                        )
-                    ),
-                    shape = RoundedCornerShape(15.dp)
-                )
-
-
+        Image(
+            painter = painterResource(id = com.example.composefebrury.R.mipmap.image_marker),
+            contentDescription = null,
+            modifier = Modifier.fillMaxWidth()
+                .align(Alignment.Center)
+        )
+        Icon(
+            painter = painterResource(id = com.example.composefebrury.R.drawable.ic_launcher_foreground),
+            contentDescription = null,
+            modifier = Modifier.align(Alignment.Center)
         )
     }
 }
