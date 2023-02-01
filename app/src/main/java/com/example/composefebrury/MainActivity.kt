40a87e74e437aa3e269bb0e5fb73bff1571dfef9
matr1x244 <zio-21@yandex.ru>
2023-02-01 20:45:00 +0300

    Добавил проверку на количество кликов if else

--- app/src/main/java/com/example/composefebrury/MainActivity.kt
+++ app/src/main/java/com/example/composefebrury/MainActivity.kt
@@ -4,11 +4,7 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.Modifier
-import com.example.composefebrury.home.HomeScreen
 import com.example.composefebrury.home.StateScreen
 
 class MainActivity : ComponentActivity() {
@@ -21,12 +17,12 @@ class MainActivity : ComponentActivity() {
 
             StateScreen(counter = countClickers, onCounterClickers = {
                 countClickers.value++
-                Toast.makeText(this, "${countClickers.value}", Toast.LENGTH_SHORT).show()
+                if (countClickers.value > 10)
+                    println("Больше 10 кликов.. кликов уже ${countClickers.value}")
+                else
+                    Toast.makeText(this, "${countClickers.value}", Toast.LENGTH_SHORT).show()
             })
         }
     }
 
-    inner class Counter() {
-        var value = 0
-    }
 }
